af70fb4af2280553a83dd22bd9647fb9b41a3b9a
tamal <tamal@appscode.com>
2018-02-19 22:32:39 -0800

    Add notifier for interceptors

---
+++ notifier/interceptors.go
@@ -0,0 +1,31 @@
+package notifier
+
+import (
+	utilerrors "github.com/appscode/go/util/errors"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+// UnaryServerInterceptor wraps a unary server with error notifier
+//
+// Invalid messages will be rejected with `Internal` before reaching any userspace handlers.
+func UnaryServerInterceptor(fn grpc.UnaryServerInterceptor) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		v, err := fn(ctx, req, info, handler)
+		if err != nil {
+			utilerrors.HandleError(err)
+		}
+		return v, err
+	}
+}
+
+// StreamServerInterceptor wraps a unary server with error notifier
+func StreamServerInterceptor(fn grpc.StreamServerInterceptor) grpc.StreamServerInterceptor {
+	return func(srv interface{}, stream grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+		err := fn(srv, stream, info, handler)
+		if err != nil {
+			utilerrors.HandleError(err)
+		}
+		return err
+	}
+}
--- security/interceptors.go
+++ security/interceptors.go
@@ -1,4 +1,4 @@
-package endpoints
+package security
 
 import (
 	"golang.org/x/net/context"
